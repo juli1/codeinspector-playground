8f3b3609079c5c4856d6114633b4a82ef907a969
Julien Delange <julien@gunnm.org>
2019-07-22 22:53:39 -0700

    add even more functions, up to 6

--- pythoncode2.py
+++ pythoncode2.py
@@ -7,5 +7,14 @@ def func2(arg):
 def func3(arg):
   print("super arg {0}".format(arg))
 
+def func4(arg):
+  print("super arg {0}".format(arg))
+
+def func5(arg):
+  print("super arg {0}".format(arg))
+
+def func6(arg):
+  print("super arg {0}".format(arg))
+
 func("bla")
 
