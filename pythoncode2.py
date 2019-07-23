b35dc01fb5a6d7db1aff46e928f1cf76c9d3fd83
Julien Delange <julien@gunnm.org>
2019-07-22 21:45:06 -0700

    update

--- pythoncode2.py
+++ pythoncode2.py
@@ -1,5 +1,11 @@
 def func(arg):
   print("super arg {0}".format(arg))
 
+def func2(arg):
+  print("super arg {0}".format(arg))
+
+def func3(arg):
+  print("super arg {0}".format(arg))
+
 func("bla")
 
