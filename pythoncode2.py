a53156dfaa9c763dddc767602c401fb6dadae48d
Julien Delange <julien@gunnm.org>
2019-07-29 22:16:17 -0700

    add function9

--- pythoncode2.py
+++ pythoncode2.py
@@ -33,6 +33,13 @@ def func8(arg, arg2):
   """
   print("super arg {0}".format(arg))
 
+def func9(arg, arg2):
+  """
+  blabla
+  :arg: bla
+  """
+  print("super arg {0}".format(arg))
+
 
 
 func("bla")
