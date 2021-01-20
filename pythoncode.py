e544c3919377496882166f269df59b75088866f1
Julien <julien@gunnm.org>
2021-01-19 18:01:07 -0700

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -7,8 +7,8 @@ def func2(arg):
   print("super arg {0}".format(arg))
 
 def func3(arg):
-  logging.info("hello world".format(arg))
-  print("super arg {0}".format(arg))
+    logging.info("hello world %s", arg)
+    print("super arg {0}".format(arg))
 
 func("bla")
 func2("bla")
