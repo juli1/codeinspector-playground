67cf7d3473047ce407fc85d7ef7e8d7f7af79d6a
Julien Delange <julien@gunnm.org>
2021-05-29 22:07:51 -0600

    bla

--- pythoncode7.py
+++ pythoncode7.py
@@ -4,9 +4,6 @@ def func(arg):
 def func2(arg):
   print("super arg {0}".format(arg))
 
-def func3(arg):
-  print("super arg {0}".format(arg))
-
 def func4(arg):
   print("super arg {0}".format(arg))
 
