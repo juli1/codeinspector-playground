2ec9520ec40934864fc13dab2955b7dc38e8b64c
Julien <julien@gunnm.org>
2022-01-06 10:35:36 -0700

    Update pythoncode9.py

--- pythoncode9.py
+++ pythoncode9.py
@@ -4,8 +4,6 @@ def func(arg):
   logging.info("blabla {0}".format(arg))
   print("super arg {0}".format(arg))
 
-def func2(arg):
-  print("super arg {0}".format(arg))
 
 def func3(arg):
   print("super arg {0}".format(arg))
