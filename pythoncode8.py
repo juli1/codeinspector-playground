a8752800fc196bdadf52c27332066bad643d4b5e
Julien <julien@gunnm.org>
2022-02-07 08:50:16 -0700

    Update pythoncode8.py

--- pythoncode8.py
+++ pythoncode8.py
@@ -1,7 +1,6 @@
 import logging
 
 def func(arg):
-  logging.info("blabla {0}".format(arg))
   print("super arg {0}".format(arg))
 
 def func2(arg):
@@ -30,38 +29,6 @@ def func9(arg):
 
 def func10(arg):
   print("super arg {0}".format(arg))
-
-def func7(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func8(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func9(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func12(arg, arg2):
-  print("super arg {0}".format(arg))
-
-def func10(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
 
 
 
