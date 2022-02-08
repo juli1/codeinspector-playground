13eb9ace9344f6e5d07497d91adcece4d6012fe5
Julien <julien@gunnm.org>
2022-02-07 17:15:35 -0700

    Update pythoncode9.py

--- pythoncode9.py
+++ pythoncode9.py
@@ -29,38 +29,6 @@ def func9(arg):
 def func10(arg):
   print("super arg {0}".format(arg))
 
-def func7(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func8(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func9(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-def func12(arg, arg2):
-  print("super arg {0}".format(arg))
-
-def func10(arg, arg2):
-  """
-  blabla
-  :arg: bla
-  """
-  print("super arg {0}".format(arg))
-
-
 
 
 func("bla")
