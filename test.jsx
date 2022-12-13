fbcdad0952db6c49b83d3ee382c946bfbbe4ceae
Julien Delange <julien@gunnm.org>
2022-12-13 11:26:07 -0700

    add jsx

---
+++ test.jsx
@@ -0,0 +1,21 @@
+ class SummaryContainer extends React.Component {
+   constructor(props) {
+     super(props);
+
+     this.state = {
+       subject: 'Summary Container!!'
+     };
+   }
+
+   render() {
+     return (
+       <ul
+			 id="js-outcome-summary-container-{$objectType}-{$objectId}"
+       className="js-outcome-summary-container j-outcome-summary-container js-ed-{$objectType}-{$objectId}"
+       data-object-type="{$objectType}"
+       data-object-id="{$objectId}"
+       role="group">
+       </ul>
+     );
+   }
+ }
